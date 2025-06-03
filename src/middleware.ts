7e5ae6fac946858657f7cff772c7c440aac72401
Achille DAVID <davidachille18@gmail.com>
2025-06-03 16:51:46 +0200

    fix(next): switched version

--- src/middleware.ts
+++ src/middleware.ts
@@ -43,10 +43,8 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    runtime: "nodejs",
     matcher: [
         '/account',
         '/api/:path*',
-        '/((?!api/auth|_next/static|_next/image|favicon.ico).*)'
     ]
 };
